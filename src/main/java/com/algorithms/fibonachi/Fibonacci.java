a95021af3b8b0d6f1aada4260d7a276d64efa343
amoiseen <amoiseen@jcp.com>
2020-04-30 00:15:07 +0300

    Updated fibonacci algorithm

--- src/main/java/com/algorithms/fibonachi/Fibonacci.java
+++ src/main/java/com/algorithms/fibonachi/Fibonacci.java
@@ -2,8 +2,11 @@ package com.algorithms.fibonachi;
 
 public class Fibonacci {
     public static int getFibonacci(int n) throws IllegalArgumentException {
-        if (n < 1)
-            throw new IllegalArgumentException("n should be at least 1");
+        if (n < 0)
+            throw new IllegalArgumentException("n should be at least 0");
+
+        if (n == 0 || n == 1)
+            return n;
 
         System.out.println("b=1");
 
